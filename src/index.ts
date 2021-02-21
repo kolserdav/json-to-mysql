640afdc67580bfc7d30a7ddedcde1c2f50d2a51c
kolserdav <serega12101983@gmail.com>
2021-02-21 09:39:11 +0000

    Add docs

--- src/index.ts
+++ src/index.ts
@@ -7,29 +7,40 @@ import * as T from './types';
 
 const module = new Module();
 
+/**
+ * Регулярное выражение названия архива прайсов
+ */
 const priceRegex = /^\d+\.\d+\.\d+\-\d+ ?:?\d+\-price.tar.gz$/ 
 
+// Парсит папку с данными
 const dir = module.parseDataDir();
 (async () => {
+  // Проходит по элементам папки
   for (let i = 0; dir[i]; i++) {
     const fileName = dir[i];
+    // Если это прайс
     if (priceRegex.test(fileName)) {
+      // Извлекает архив и получает данные по прайсам
       await module.extractArchive(fileName, 'price');
       const fileData = await module.getFileData<T.SourcePrices>('price');
       Console.info(`Write in table "Price" from archive "${fileName}"...`);
+      // Записывает каждый прайс
       for (let prop in fileData) {
         const price = fileData[prop];
-        //await module.saveOnePrice(price, fileName);
+        await module.saveOnePrice(price, fileName);
       }
       Console.info(`Prices from archive "${fileName}" write successfully!`);
-      await module.extractArchive(fileName.replace('price', 'sellers'), 'sellers');
+      // Получает имя архива продавцов из имени архива прайсов
+      const sellersFilename = fileName.replace('price', 'sellers');
+      // Делает то же что и по прайсам, то есть пишет в базу но в свою таблицу
+      await module.extractArchive(sellersFilename, 'sellers');
       const fileSellersData = await module.getFileData<T.SourceSeller[]>('sellers');
+      Console.info(`Write in table "Sellers" from archive "${sellersFilename}"...`);
       for (let i = 0; fileSellersData[i]; i++) {
         const seller = fileSellersData[i];
         await module.saveOneSeller(seller, fileName);
       }
-      await module.disconnect();
-      break;
     }
   }
+  await module.disconnect();
 })();
